695270633d224aff6cc5ad0893e178daf4c10354
OceaneLao <oceane.lao1@gmail.com>
2024-03-11 15:44:06 +0100

    function responsive mobile first about and homepage

--- about_me.js
+++ about_me.js
@@ -1,4 +1,4 @@
-// About container
+// Création d'un container pour le bloc About
 let aboutContainer = document.createElement("div");
 aboutContainer.setAttribute(
   "class",
@@ -9,26 +9,28 @@ document.body.appendChild(aboutContainer).style.marginBottom = "200px";
 
 // Display container picture, text and button
 let aboutDisplay = document.createElement("div");
-aboutDisplay.setAttribute(
-  "class", 
-  "aboutDisplay d-lg-flex");
+aboutDisplay.setAttribute("class", "aboutDisplay d-lg-flex");
 document.getElementsByClassName("aboutContainer")[0].appendChild(aboutDisplay);
 
-// Display picture
-let pictureAbout = document.createElement("img");
-pictureAbout.src = "assets/img/Round_design.png";
-document.getElementsByClassName("aboutDisplay")[0].appendChild(pictureAbout);
-document
-  .getElementsByClassName("aboutDisplay")[0]
-  .appendChild(pictureAbout).style.maxWidth = "25%";
-document
-  .getElementsByClassName("aboutDisplay")[0]
-  .appendChild(pictureAbout).style.maxHeight = "25%";
-document
-  .getElementsByClassName("aboutDisplay")[0]
-  .appendChild(pictureAbout).style.marginRight = "15px";
+// Créer une fonction pour afficher tout le contenu About (image, titre, texte et bouton)
+function displayContent() {
+  // Récupérer l'élément parent pour pouvoir le modifier
+  let displayPicture = document.getElementsByClassName("aboutDisplay")[0];
 
-// Title, text and button
+  // Effacer le contenu de l'élément parent
+  displayPicture.innerHTML = "";
+
+  // Propriétés de l'image en version Desktop
+  let pictureAbout = document.createElement("img");
+  pictureAbout.src = "assets/img/Round_design.png";
+  pictureAbout.style.maxWidth = "25%";
+  pictureAbout.style.maxHeight = "25%";
+  pictureAbout.style.marginRight = "15px";
+
+  //Afficher l'image dans le navigateur
+  displayPicture.appendChild(pictureAbout);
+
+  // Créer une div pour contenir le titre, le texte et le bouton
 let titleTextButton = document.createElement("div");
 titleTextButton.setAttribute(
   "class",
@@ -36,41 +38,49 @@ titleTextButton.setAttribute(
 );
 document.getElementsByClassName("aboutDisplay")[0].appendChild(titleTextButton);
 
-// Title H2
+// Titre H2
 let titleAbout = document.createElement("h2");
 titleAbout.textContent = "A propos de moi";
-titleAbout.setAttribute(
-  "class", 
-  "text-center text-light mt-1"
-  );
+titleAbout.setAttribute("class", "text-center text-light mt-1");
 document.getElementsByClassName("titleTextButton")[0].appendChild(titleAbout);
 
-// Text about
+// Texte à propos
 let textAbout = document.createElement("p");
-textAbout.setAttribute("class", "textAbout text-center");
+textAbout.setAttribute("class", "textAbout text-center text-light");
 textAbout.textContent =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 document
   .getElementsByClassName("titleTextButton")[0]
-  .appendChild(textAbout).style.color = "#fff";
+  .appendChild(textAbout);
 
-// Add button Contact Me container
+// Ajouter le bouton "Contactez-moi"
 let buttonContactMe = document.createElement("a");
 buttonContactMe.setAttribute("class", "text-decoration-none");
 buttonContactMe.href = "#";
 buttonContactMe.textContent = "Contactez-moi";
 document
   .getElementsByClassName("titleTextButton")[0]
-  .appendChild(buttonContactMe).style.color = "#010028";
-document
-  .getElementsByClassName("titleTextButton")[0]
-  .appendChild(buttonContactMe).style.backgroundColor = "#fff";
-document
-  .getElementsByClassName("titleTextButton")[0]
-  .appendChild(buttonContactMe).style.padding = "15px 50px";
-document
-  .getElementsByClassName("titleTextButton")[0]
-  .appendChild(buttonContactMe).style.borderRadius = "50px";
-document
-  .getElementsByClassName("titleTextButton")[0]
-  .appendChild(buttonContactMe).style.boxShadow = "5px 5px 15px #9F23C1";
+  .appendChild(buttonContactMe);
+buttonContactMe.style.color = "#010028";
+buttonContactMe.style.backgroundColor = "#fff";
+buttonContactMe.style.padding = "15px 50px";
+buttonContactMe.style.borderRadius = "50px";
+buttonContactMe.style.boxShadow = "5px 5px 15px #9F23C1";
+
+  // Version Mobile first
+  let isMobileAbout = window.innerWidth <= 767;
+  if (isMobileAbout) {
+    //Propriétés de l'image en mobile first
+    pictureAbout.style.maxWidth = "100%";
+    pictureAbout.style.maxHeight = "100%";
+    pictureAbout.style.marginRight = "0px";
+    //Propriétés du bouton en mobile first
+    buttonContactMe.style.padding = "15px 20px";
+  }
+}
+// Appeler la fonction
+displayContent();
+// Ajouter un gestionnaire d'évènement lors du redimensionnement
+window.addEventListener("resize", function () {
+  displayContent();
+});
--- homepage.js
+++ homepage.js
@@ -1,66 +1,47 @@
-// Function to set the homepage background image according to the width of the screen
-function setHomepageBackgroundImage() {
-  let isMobile = window.innerWidth <= 767;
+// Créer une fonction pour afficher tout le contenu de Homepage
+let homepageContainer = document.createElement("div");
+homepageContainer.setAttribute(
+  "class",
+  "homepage-container container d-flex flex-column justify-content-center"
+);
 
-  let homepageContainer = document.createElement("div");
-  homepageContainer.setAttribute(
-    "class",
-    "container d-flex flex-column justify-content-center"
-  );
-
+// Propriétés de l'image en arrière plan pour le bloc Homepage
   let homepageBackgroundImage = "url('assets/img/Vertical_block.png')";
   document.body.appendChild(homepageContainer).style.backgroundImage =
     homepageBackgroundImage;
-  if (isMobile) {
-  } else {
-    document.body.appendChild(homepageContainer).style.backgroundPosition =
-      "center";
-    document.body.appendChild(homepageContainer).style.backgroundRepeat =
-      "no-repeat";
-    document.body.appendChild(homepageContainer).style.marginTop = "50px";
-    document.body.appendChild(homepageContainer).style.height = "645px";
-    document.body.appendChild(homepageContainer).style.backgroundSize =
-      "contain";
-  }
+  document.body.appendChild(homepageContainer).style.backgroundPosition =
+    "center";
+  document.body.appendChild(homepageContainer).style.backgroundRepeat =
+    "no-repeat";
+  document.body.appendChild(homepageContainer).style.marginTop = "50px";
+  document.body.appendChild(homepageContainer).style.height = "645px";
+  document.body.appendChild(homepageContainer).style.backgroundSize = "contain";
 
-  // Display homepage content
+  // Contenu de Homepage
   let homepageContent = document.createElement("div");
   homepageContent.setAttribute("class", "homepage-content");
   document
     .getElementsByClassName("container")[0]
     .appendChild(homepageContent).style.marginLeft = "100px";
-}
 
-setHomepageBackgroundImage();
+  // Titre H1 Homepage / slogan
+  let newTitle = document.createElement("h1");
+  newTitle.textContent = "The best moment is yet to come - BTS";
+  newTitle.setAttribute("class", "col-5 text-light align-items-start mb-5");
+  document.getElementsByClassName("homepage-content")[0].appendChild(newTitle);
 
-window.addEventListener("resize", setHomepageBackgroundImage);
-
-// Display H1 tilte on homepage container
-let newTitle = document.createElement("h1");
-newTitle.textContent = "The best moment is yet to come - BTS";
-newTitle.setAttribute("class", "col-5 text-light align-items-start mb-5");
-document.getElementsByClassName("homepage-content")[0].appendChild(newTitle);
-
-// Add button on homepage container
-let buttonDiscover = document.createElement("a");
-buttonDiscover.setAttribute(
-  "class",
-  "col-2 text-decoration-none text-center align-items-center"
-);
-buttonDiscover.href = "#";
-buttonDiscover.textContent = "Découvrir";
-document
-  .getElementsByClassName("homepage-content")[0]
-  .appendChild(buttonDiscover).style.color = "#010028";
-document
-  .getElementsByClassName("homepage-content")[0]
-  .appendChild(buttonDiscover).style.backgroundColor = "#fff";
-document
-  .getElementsByClassName("homepage-content")[0]
-  .appendChild(buttonDiscover).style.padding = "15px 50px";
-document
-  .getElementsByClassName("homepage-content")[0]
-  .appendChild(buttonDiscover).style.borderRadius = "50px";
-document
-  .getElementsByClassName("homepage-content")[0]
-  .appendChild(buttonDiscover).style.boxShadow = "5px 5px 15px #9F23C1";
+  // Bouton Homepage
+  let buttonDiscover = document.createElement("a");
+  buttonDiscover.setAttribute(
+    "class",
+    "col-2 text-decoration-none text-center align-items-center"
+  );
+  buttonDiscover.href = "#";
+  buttonDiscover.textContent = "Découvrir";
+  document
+    .getElementsByClassName("homepage-content")[0]
+    .appendChild(buttonDiscover).style.color = "#010028";
+  buttonDiscover.style.backgroundColor = "#fff";
+  buttonDiscover.style.padding = "15px 50px";
+  buttonDiscover.style.borderRadius = "50px";
+  buttonDiscover.style.boxShadow = "5px 5px 15px #9F23C1";
