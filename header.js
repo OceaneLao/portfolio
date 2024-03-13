6bd06c2c0e6f48e63c6ce18a07a1d87cd0b5b6f1
Lao Oceane <oceane.lao1@gmail.com>
2024-03-13 11:27:05 +0100

    add links social media contact

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-// HEADER
+// Créer la balise header contenant le bloc de l'en-tête de la page
 let header = document.createElement("header");
 header.setAttribute(
   "class",
@@ -6,8 +6,9 @@ header.setAttribute(
 );
 document.body.appendChild(header).style.backgroundColor = "#010028";
 
+// Créer une fonction contenant l'affichage du header
 function displayHeader() {
-  // HEADER LIST NAVBAR with title and URL (link)
+  // Créer une liste (li) contenant les onglets avec les liens (a href)
   let menu = [
     {
       title: "Accueil",
@@ -27,11 +28,12 @@ function displayHeader() {
     },
   ];
 
-  // Use loop method to browse an array (list)
+  // Utilisation du for pour parcourir le tableau
   for (let i = 0; i < menu.length; i++) {
+    // Créer la liste des onglets
     let liMenu = document.createElement("li");
 
-    // Create a link
+    // Créer des liens (a href)
     let link = document.createElement("a");
     link.href = menu[i].url;
     link.textContent = menu[i].title;
@@ -40,10 +42,10 @@ function displayHeader() {
     header.appendChild(liMenu).style.listStyleType = "none";
     link.setAttribute("class", "ms-3 me-3 text-light text-decoration-none");
 
-    // Déclarer une variable en version Mobile First et ajouter une condition
+    // Déclarer une variable en version Mobile First et ajouter une condition pour le redimensionnement
     let isMobileHeader = window.innerWidth <= 767;
     if (isMobileHeader) {
-      // Récupérer l'élément parent pour le rendre vide : cela permettra de modifier le contenu à l'intérieur et éviter de répéter les éléments à l'intérieur
+      // Récupérer l'élément parent pour le rendre vide : cela permettra de modifier le contenu à l'intérieur et d'éviter de répéter les éléments à l'intérieur
       let headerIconBurgerDisplay =
         document.getElementsByClassName("header-display")[0];
       headerIconBurgerDisplay.innerHTML = "";
