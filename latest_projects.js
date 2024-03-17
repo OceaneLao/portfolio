a0042ec6f0c4250ebfc6d38d7dbf67b94152cdf9
Lao Oceane <oceane.lao1@gmail.com>
2024-03-17 14:52:58 +0100

    latest-projects file js

--- latest_projects.js
+++ latest_projects.js
@@ -1,61 +1,62 @@
+// Afficher le titre H2
+let titleLatestProjects = document.createElement("h2");
+titleLatestProjects.textContent = "Mes derniers projets";
+titleLatestProjects.setAttribute("id","latest-projects");
+titleLatestProjects.setAttribute("class", "text-center text-light mt-5 mb-5");
+document.body.appendChild(titleLatestProjects);
+
 // Créer une balise contenant le bloc Mes Derniers Projets
 let latestProjectsContainer = document.createElement("div");
-latestProjectsContainer.setAttribute("id", "latest-projects");
 latestProjectsContainer.setAttribute(
   "class",
   "latestProjectsContainer container w-lg-75 w-sm-100 d-flex flex-wrap justify-content-center align-items-center"
 );
 document.body.appendChild(latestProjectsContainer);
 
-// Créer une div contenant le titre H2 et l'afficher
-let displayTitleLatestProjects = document.createElement("div");
-displayTitleLatestProjects.setAttribute("class", "title-h2 col-12 pt-3");
-document.getElementById("latest-projects").appendChild(displayTitleLatestProjects);
+// Créer une fonction pour afficher les cards "Mes derniers projets"
+function displayCards(cards) {
+  // Parcourir les données dans le fichier JSON
+  cards.forEach((card) => {
+    // Affichage des cards
+    let cardDisplay = document.createElement("div");
+    cardDisplay.setAttribute(
+      "id",
+      "cardDisplay"
+    );
+    cardDisplay.setAttribute(
+      "class",
+      "cardDisplay flex-column col-6 text-light text-left"
+    );
+    document.getElementsByClassName("container")[1].appendChild(cardDisplay);
 
-let titleLatestProjects = document.createElement("h2");
-titleLatestProjects.textContent = "Mes derniers projets";
-titleLatestProjects.setAttribute("class", "text-center text-light mt-5 mb-5");
-document.getElementsByClassName("title-h2")[0].appendChild(titleLatestProjects);
- 
-// Fetch JSON file
+    let pictureCard = new Image();
+    pictureCard.src = card.picture;
+    pictureCard.style.maxWidth = "100%";
+    pictureCard.style.maxHeight = "50%";
+
+    let titleCard = document.createElement("h3");
+    titleCard.textContent = card.title;
+
+    let dateCard = document.createElement("p");
+    dateCard.textContent = card.date;
+
+    let descriptionCard = document.createElement("p");
+    descriptionCard.textContent = card.description;
+
+    // Afficher les données dans le fichier HTML
+    cardDisplay.appendChild(pictureCard);
+    cardDisplay.appendChild(titleCard);
+    cardDisplay.appendChild(dateCard);
+    cardDisplay.appendChild(descriptionCard);
+  });
+}
+
+// Fetch du fichier JSON pour n'afficher que les 2 premières données
 document.addEventListener("DOMContentLoaded", function () {
   fetch("projects.json")
-    .then((res) => {
-      return res.json();
-    })
+    .then((res) => res.json())
     .then((cards) => {
-      // Sélectionner les données dans le fichier JSON
-      cards.slice(-2).forEach((card) => {
-        // Affichage des cards
-        let cardDisplay = document.createElement("div");
-        cardDisplay.setAttribute(
-          "class",
-          "cardDisplay flex-column col-6 text-light text-left"
-        );
-        document
-          .getElementsByClassName("container")[1]
-          .appendChild(cardDisplay);
-
-        let pictureCard = new Image();
-        pictureCard.src = card.picture;
-        pictureCard.style.maxWidth = "100%";
-        pictureCard.style.maxHeight = "50%";
-
-        let titleCard = document.createElement("h3");
-        titleCard.textContent = card.title;
-
-        let dateCard = document.createElement("p");
-        dateCard.textContent = card.date;
-
-        let descriptionCard = document.createElement("p");
-        descriptionCard.textContent = card.description;
-
-        // Afficher les données dans le fichier HTML
-        cardDisplay.appendChild(pictureCard);
-        cardDisplay.appendChild(titleCard);
-        cardDisplay.appendChild(dateCard);
-        cardDisplay.appendChild(descriptionCard);
-      });
+      displayCards(cards.slice(0, 2));
     })
     .catch((error) => console.error("Unable to fetch data:", error));
 });
@@ -67,10 +68,9 @@ displayButtonSeeMore.setAttribute(
   "buttonSeeMore d-flex justify-content-center align-items-center"
 );
 document.body.appendChild(displayButtonSeeMore);
-
 let buttonSeeMore = document.createElement("a");
 buttonSeeMore.setAttribute("class", "text-decoration-none mt-3");
-
+buttonSeeMore.href = "#cardDisplay";
 buttonSeeMore.textContent = "En voir plus";
 document
   .getElementsByClassName("buttonSeeMore")[0]
@@ -81,6 +81,14 @@ buttonSeeMore.style.borderRadius = "50px";
 buttonSeeMore.style.boxShadow = "5px 5px 15px #9F23C1";
 buttonSeeMore.style.position = "center";
 
-buttonSeeMore.addEventListener('click', function() {
-  buttonSeeMore.href = cards.push(2).forEach((card));
-} );
+// Ajouter un gestionnaire d'évènements qui va générer les autres données dans le fichier JSON lorsque le bouton sera cliqué
+buttonSeeMore.addEventListener("click", function () {
+  fetch("projects.json")
+    .then((res) => res.json())
+    .then((cards) => {
+      displayCards(cards.slice(2));
+      // A surveiller si plus de projets + test
+      buttonSeeMore.style.display = "none";
+    })
+    .catch((error) => console.error("Unable to fetch data:", error));
+});
